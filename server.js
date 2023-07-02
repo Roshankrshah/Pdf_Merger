ff929f86c1e962c52dda1687fb4e861b3ff0b350
Roshan <shahroshan0720@gmail.com>
2023-07-02 15:34:59 +0530

    setup pdf merger

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express();
 
 PORT = 3001;
@@ -8,6 +10,11 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"templates/index.html"));
 })
 
+app.post('/merge',upload.array('pdfs',2),function(req,res,next){
+    console.log(req.files);
+    res.send({data: req.files});
+
+})
 app.listen(PORT,()=>{
     console.log(`Sever listening on http://localhost:${PORT}`);
 });
